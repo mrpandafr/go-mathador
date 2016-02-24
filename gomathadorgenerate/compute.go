c4cb4abff2abc3a9b78abb20d20b7d2381b4309b
mrpandafr <jeansebastien.saillard@gmail.com>
2016-02-24 10:40:27 +0100

    prep for rendering data in database might be more faster than retrieve from sqlite

---
+++ gomathadorgenerate/compute.go
@@ -0,0 +1,11 @@
+package gomathadorgenerate
+
+import "github.com/mrpandafr/go-mathador/model"
+
+func compute(t model.Tirage) model.Tirage {
+	return t
+}
+
+func order(t model.Tirage) model.Tirage {
+	return t
+}
